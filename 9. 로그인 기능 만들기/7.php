8fcd508eaeca5b827202a0cc43a95c72be3ed02e
DH Kim <inerplat@ssu.ac.kr>
2017-01-20 23:00:05 +0900

    login

---
+++ 9. 로그인 기능 만들기/7.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+
+  </head>
+  <body>
+    <script type="text/javascript">
+      password = prompt("비밀번호");
+      if (password=="1234")
+      {
+        document.write("로그인 성공");
+      }
+      else
+      {
+        document.write("로그인 실패");
+      }
+    </script>
+  </body>
+</html>
