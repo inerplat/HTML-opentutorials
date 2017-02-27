f73ef3e1afcdb16cd8d366e25b13008b0dc2d783
DH Kim <inerplat@gmail.com>
2017-02-27 18:23:23 +0900

    basic of PHP & Javascript

---
+++ 05-03. 웹페이지에 코드 삽입하기/1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+  <html>
+
+    <head>
+      <meta charset = "utf-8" />
+    </head>
+
+    <body>
+      <h1>PHP</h1>
+      <?php
+        echo "Hello World";
+       ?>
+       <h1>JavaScript</h1>
+       <script type="text/javascript">
+         document.write("Hello World");
+       </script>
+    </body>
+
+  </html>
