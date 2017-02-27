6d2f5bd36cf33807551f34f5ea30aeefcc070ae0
DH Kim <inerplat@gmail.com>
2017-02-27 18:22:08 +0900

    rename

--- 03-2. 박스모델/readme.md
+++
@@ -1,2 +0,0 @@
-#박스 모델
-<img src="http://inerplat.esy.es/github_img/boxmodel.gif">
--- 17. 관계형 데이터베이스 실습/d_process.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $conn = mysqli_connect("o","o","o");
-  mysqli_select_db($conn,'webapp');
-  $result= mysqli_query($conn,'SELECT * FROM topic');
-
-  $sql = "DELETE FROM topic WHERE title=".'"'.$_POST['title'].'"';
-  $result = mysqli_query($conn,$sql);
-  header('Location:http://localhost/index.php');
- ?>
