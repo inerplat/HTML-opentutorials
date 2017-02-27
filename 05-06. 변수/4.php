5dc8b5424e15cb7ca4a00b91f9e3355930bf5931
DH Kim <inerplat@gmail.com>
2017-02-27 18:23:53 +0900

    variable

---
+++ 05-06. 변수/4.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>
+    </title>
+  </head>
+  <body>
+    <h1>JavaScript</h1>
+    <script type="text/javascript">
+      tmp = "DH Kim";
+      document.write("안녕하세요 "+tmp);
+    </script>
+    <h1>PHP</h1>
+    <?php
+    $tmp = "DH Kim";
+    echo "안녕하세요 ".$tmp;
+     ?>
+  </body>
+</html>
